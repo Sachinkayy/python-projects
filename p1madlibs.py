4f32c0318a1083e48ae25e44e4090a55a38873cd
Sachin <{ID}+{username}@users.noreply.github.com>
2022-02-25 18:42:52 +0530

    initial commit,created p1 madlibs.py

---
+++ p1madlibs.py
@@ -0,0 +1,13 @@
+'''this is the project 1, here we do a fun game for kids, in which
+kids have blanks to fill and program will use those words to 
+make a story'''
+
+adj = input("Adjective ")
+verb1= input("put a verb ")
+verb2=input("put verb again ")
+famous_person = input("enter your fav superhero ")
+
+madlib = f" \n Computer programming is so {adj}, it makes me so excited all \
+the time. I love to {verb1}. Stay hydrated and {verb2} do like you are {famous_person}"
+
+print(madlib)
